6acacada3cb6227f868e20216a0ba73ba8b57b17
Sphesihle Mhlongo <mhlongosihle49@gmail.com>
2025-01-21 10:39:30 +0200

    port

--- app.py
+++ app.py
@@ -255,4 +255,5 @@ def respond():
     return jsonify({"response": response})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = os.environ.get("PORT", 5000)  # Default to 5000 if not set
+    app.run(debug=True, host="0.0.0.0", port=port)
